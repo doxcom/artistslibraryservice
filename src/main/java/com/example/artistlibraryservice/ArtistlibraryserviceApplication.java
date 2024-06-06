6758abb17cd896c262455c39160bb03cfdf18c67
doxcom <aldoperez1319@gmail.com>
2024-06-06 15:28:17 -0600

    initial files

---
+++ src/main/java/com/example/artistlibraryservice/ArtistlibraryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.artistlibraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistlibraryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistlibraryserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artistlibraryservice/ArtistlibraryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artistlibraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistlibraryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
